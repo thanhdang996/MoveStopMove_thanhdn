d27f41f03c7c1804177621848504ee7023f3a452
thanhdang996 <blackholeyoshitsune@gmail.com>
2023-03-02 18:47:41 +0700

    add bullet, camera follow

---
+++ Assets/_Game/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 20;
+    [SerializeField] private float timeDestroy = 0.5f;
+    [SerializeField] private float scalePerKill = 0.1f;
+    private Character sourceFireCharacter;
+    public Character SourceFireCharacter { get => sourceFireCharacter; set => sourceFireCharacter = value; }
+    private Rigidbody rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void MoveAndAutoDestroy()
+    {
+        rb.AddForce(speed * transform.forward, ForceMode.Impulse);
+        Invoke(nameof(OnDespawn), timeDestroy);
+    }
+
+    private void OnDespawn()
+    {
+        rb.velocity= Vector3.zero;
+        SourceFireCharacter = null;
+        if (!gameObject.activeSelf) return;
+        ObjectPooling.Instance.ReturnBullet(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Character character))
+        {
+            SourceFireCharacter.CharactersTargeted.Remove(character);
+            Vector3 oriScale = SourceFireCharacter.transform.localScale;
+            SourceFireCharacter.transform.localScale = oriScale + (Vector3.one * scalePerKill);
+            character.OnDespawn();
+            OnDespawn();
+        }
+    }
+}
