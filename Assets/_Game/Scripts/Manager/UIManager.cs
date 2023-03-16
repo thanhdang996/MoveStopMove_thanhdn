f9904f7b15f4f7cf9082624501a9689491a7db0e
thanhdang996 <blackholeyoshitsune@gmail.com>
2023-03-16 17:19:23 +0700

    add first logic UI add coint

--- Assets/_Game/Scripts/Manager/UIManager.cs
+++ Assets/_Game/Scripts/Manager/UIManager.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private FixedJoystick joystick;
     public FixedJoystick Joystick => joystick;
+
+    [SerializeField] private TextMeshProUGUI coinText;
+    [SerializeField] private GameObject popUpSetting;
+
+    private void Start()
+    {
+        UpdateCoinText();
+    }
+
+
+    public void UpdateCoinText()
+    {
+        coinText.text = GameManager.Instance.Data.Coin.ToString();
+    }
+
+    public void ShowPopupSetting()
+    {
+        popUpSetting.SetActive(true);
+    }
 }
