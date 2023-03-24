94be6a1012394a7ff0b9fee831246889d1f0b873
thanhdang996 <blackholeyoshitsune@gmail.com>
2023-03-24 15:19:26 +0700

    -add asset music clip
    -add music
    - add tag name for obstacle level2

--- Assets/_Game/Scripts/CONSTANT/Constant.cs
+++ Assets/_Game/Scripts/CONSTANT/Constant.cs
@@ -13,18 +13,12 @@ public class Constant : MonoBehaviour
     public const string VOLUME_MY_MUSIC = "MyMusic";
     public const string VOLUME_MY_SFX = "MySFX";
 
-    //public static MyPoolType ConvertWeaponTypeeToObjectType(WeaponType weaponType)
-    //{
-    //    return weaponType switch
-    //    {
-    //        WeaponType.Axe => MyPoolType.Axe,
-    //        WeaponType.Boomerang => MyPoolType.Boomerang,
-    //        WeaponType.Cream => MyPoolType.Cream,
-    //        _ => MyPoolType.None,
-    //    };
-    //}
 
-    public static PoolType ConvertWeaponTypeeToObjectType(WeaponType weaponType)
+    public const string TAG_OBSTACLE = "Obstacle";
+
+
+
+    public static PoolType ConvertWeaponTypeToObjectType(WeaponType weaponType)
     {
         return weaponType switch
         {
