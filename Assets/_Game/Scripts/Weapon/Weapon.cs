684b2bdb2b624d60c91873214088e962b2059265
thanhdang996 <blackholeyoshitsune@gmail.com>
2023-03-24 15:28:38 +0700

    refactor weapon
    Move all logic OnTriggerEnter to Weapon class

--- Assets/_Game/Scripts/Weapon/Weapon.cs
+++ Assets/_Game/Scripts/Weapon/Weapon.cs
@@ -12,7 +12,9 @@ public class Weapon : GameUnit
     public Character SourceFireCharacter { get => sourceFireCharacter; set => sourceFireCharacter = value; }
     protected Rigidbody rb;
 
-    [SerializeField] private WeaponType weaponType;
+    [SerializeField] protected WeaponType weaponType;
+
+    [SerializeField] protected RotationSelf rotationSelf;
 
     private void Awake()
     {
@@ -21,8 +23,17 @@ public class Weapon : GameUnit
 
     public virtual void Launch()
     {
+        rotationSelf.enabled = true;
         timeStop = SourceFireCharacter.GetTimeSecondToStopWeapon(speed);
         rb.AddForce(speed * TF.forward, ForceMode.Impulse);
+    }
+
+    protected void OnHitObstacle()
+    {
+        SoundManager.Instance.PlaySoundSFX3D(SoundType.WeaponCollider, TF.position, 0);
+        rb.velocity = Vector3.zero;
+        rotationSelf.enabled = false;
+        Invoke(nameof(OnDespawn), 0.5f);
     }
 
     protected virtual void OnDespawn()
@@ -30,6 +41,28 @@ public class Weapon : GameUnit
         rb.velocity = Vector3.zero;
         SourceFireCharacter = null;
         //ObjectPooling.Instance.ReturnGameObject(gameObject, Constant.ConvertWeaponTypeeToObjectType(weaponType));
-        SimplePool.Despawn(this); 
+        SimplePool.Despawn(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Constant.TAG_OBSTACLE))
+        {
+            StopAllCoroutines();
+            OnHitObstacle();
+            return;
+        }
+
+        Character character = Cache.GetCharacter(other);
+        if (character != null)
+        {
+            if (character == SourceFireCharacter) return;
+            SoundManager.Instance.PlaySoundSFX3D(SoundType.WeaponHit, TF.position, (int)weaponType);
+            character.OnDespawn();
+            SourceFireCharacter.ChangeScalePerKillAndIncreaseLevel();
+
+            StopAllCoroutines();
+            OnDespawn();
+        }
     }
 }
